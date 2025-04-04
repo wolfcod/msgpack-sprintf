7bb6d4c497357a35a0b54524407e955e553da2dc
cod <sysenter.dev@gmail.com>
2025-04-04 07:13:10 +0200

    Added float/double/half signed format

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # msgpack_sprintf
 A sprintf function to create msgpack objects
 
-This repository contains some simple code, to allows c/c++ developers to create msgpack objects (map and array) using same syntax of printf/sprintf/scanf function.ù
+This repository contains some simple code, to allows c/c++ developers to create msgpack objects (map and array) using same syntax of printf/sprintf/scanf function.
+
+This implementation is stack based, except msgpack callback implementation, which can allocate memory through sbuffer primitives.
+
+Actually, the string is not validated as input, so in case there is an error, the output object could be corrupted (actually I'm writing in the buffer the maximum number of objects possible, then I overwrite the value with the final one).
 
 ## msgpack-c
 This source code is based on [msgpack-c](https://github.com/msgpack/msgpack-c/tree/c_master) source code. Until this project is under development, the original source code is duplicated into this repo to allow test/development. The modified files are:
@@ -24,32 +28,40 @@ MsgPack supports object serialization like JSON.
 Symbols and place-holder adopted internally for fmt syntax.
 Note: Not specifiers are supported, due binary serialization in msgpack
 
-| specifier    | Size | msgpack-object | Description |
-|--------------|------|----------------|-------------|
-| { }          | 1..n | fixmap         | Create a map that supports from 0 to 2^32-1 objects |
-|              |      | map16          | |
-|              |      | map32          | |
-| [ ]          | 1..n | fixarray       | Create an array of objects that supports from 0 to 2^32'-1 elements |
-|              |      | array16        | |
-|              |      | array32        | |
-| %s           | 0..n | fixstr         | store a value as string |
-| %S           |      |                | store a value as string, but the input is an unicode string |
-|              |      | str8           | |
-|              |      | str16          | |
-|              |      | str32          | |
-| %c           | 1    | fixstr [1]     | Store a value as fixstr of 1 byte |
-| %n           | 1    | nil            | Store a null pointer, regardless the value placed in variadic arguments |
-| %b           | 1    | false or true  | Store a boolean value, the variadic must be a value, 0 for false, any other value is true |
-| %p           | 0..n | bin8           | Store a buffer as bin8, bin16 or bin32, but it requires two arguments in variadic, a pointer and a size |
-|              |      | bin16          | |
-|              |      | bin32          | |
-| %f           | 1+4  | float32        | Store a float value |
-| %e           | 1+8  | float64        | Store a double value |
-| %i           | 1..9 | int8..int64    | Store an integer value, from 8 bit to 64bit |
-| %u           | 1..9 | UINT8..UINT64  | Store an unsigned integer value, from 8 to 64 bit. |
-| %!           | 1..n |                | A place holder used to fill an object through a callback function |
-| null         | 1    | nil            | write nil as value |
-| key          | 1..n | fixstr...      | write a key as string |
+| specifier    | Size | msgpack-object | Description | Status |
+|--------------|------|----------------|-------------|--------|
+| { }          | 1..n | fixmap         | Create a map that supports from 0 to 2^32-1 objects | TBT |
+|              |      | map16          | | TBT |
+|              |      | map32          | | TBT |
+| [ ]          | 1..n | fixarray       | Create an array of objects that supports from 0 to 2^32'-1 elements | TBT |
+|              |      | array16        | | TBT |
+|              |      | array32        | | TBT |
+| %s           | 0..n | fixstr         | store a value as string | TBT |
+| %S           |      |                | store a value as string, but the input is an unicode string | TBT |
+|              |      | str8           | | TBT |
+|              |      | str16          | | TBT |
+|              |      | str32          | | TBT |
+| %c           | 1    | fixstr [1]     | Store a value as fixstr of 1 byte | TBT |
+| %n           | 1    | nil            | Store a null pointer, regardless the value placed in variadic arguments | TBT |
+| %d           | 1    | false or true  | Store a boolean value, the variadic must be a value, 0 for false, any other value is true | TBT |
+| %p           | 0..n | bin8           | Store a buffer as bin8, bin16 or bin32, but it requires two arguments in variadic, a pointer and a size | TBT |
+|              |      | bin16          | | TBT |
+|              |      | bin32          | | TBT |
+| %hf          | 1+4  | bfloat16       | The input is a bfloat16 number, which will be converted into float and stored as float in msgpack | TBT |
+|              |      |                | The bfloat16 is mapped into a float number without rouding |
+| %f           | 1+4  | float32        | Store a float value | TBT |
+| %he          | 1+4  | float32        | The input is a float16 number, which will be converted into float and stored as float in msgpack | TBI |
+| %e           | 1+8  | float64        | Store a double value | TBT |
+| %i           | 1..9 | int8..int64    | Store an integer value, from 8 bit to 64bit | TBT |
+| %u           | 1..9 | UINT8..UINT64  | Store an unsigned integer value, from 8 to 64 bit. | TBT |
+| %!           | 1..n |                | A place holder used to fill an object through a callback function | TBT |
+| null         | 1    | nil            | write nil as value | TBI |
+| key          | 1..n | fixstr...      | write a key as string | TBT |
+
+Status definition:
+- TESTED
+- TBT: To be tested
+- TBI: To be implemented
 
 ## Examples
 ```c
@@ -90,3 +102,7 @@ msgpack_sprintf(out, "{key: [%!]}", recursive_element, (void *) 4); // call recu
 
 ## Array Recursive Expansion
 To allow format for `%!` and to keep simple logic (until the internal parsing will be improved) the recursion must be used only to generate array or map, so I discourage the use of inline array expansion because msgpack_sprintf at top level evaluate an array or a map.
+
+
+## Extensions
+msgpack supports extensions in data serialization, but actually it's not possible.
--- src/sprintf.c
+++ src/sprintf.c
@@ -23,6 +23,30 @@ typedef void (*msgpack_sprintf_callback)(msgpack_packer *pack, void *opt);
 static const char *msgpack_sprintf_map(msgpack_sprintf_context *ctx, const char *fmt);
 static const char *msgpack_sprintf_array(msgpack_sprintf_context *ctx, const char *fmt);
 
+// based on Martin Kallaman float32 https://gist.github.com/martin-kallman/5049614
+// source code from Alex Zhukov https://gist.github.com/zhuker/b4bd1fb306c7b04975b712c37c4c4075
+static void hf_to_float32(float *out, const uint16_t in)
+{
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t t3;
+
+    t1 = in & 0x7fffu;                       // Non-sign bits
+    t2 = in & 0x8000u;                       // Sign bit
+    t3 = in & 0x7c00u;                       // Exponent
+
+    t1 <<= 13u;                              // Align mantissa on MSB
+    t2 <<= 16u;                              // Shift sign bit into position
+
+    t1 += 0x38000000;                       // Adjust bias
+
+    t1 = (t3 == 0 ? 0 : t1);                // Denormals-as-zero
+
+    t1 |= t2;                               // Re-insert sign bit
+
+    *((uint32_t *) out) = t1;
+};
+
 /**
  * get_token parse the input string to find the initial sequence of a token
  * like ' aKey:' '  anotherKey'.
@@ -76,71 +100,112 @@ static void msgpack_sprintf_bulk(msgpack_packer *dst, msgpack_packer *src)
  * @param args
  * @return
  */
-static const char* msgpack_sprintf_pack_arg(msgpack_packer *pk, const char *fmt, va_list *args)
+static const char* msgpack_sprintf_pack_arg(msgpack_packer *pk, const char *fmt, va_list *ap)
 {
+    uint16_t u16;
+    float f32; double f64;
+    int32_t i32;
     uint32_t u32;
     void *ptr;
     msgpack_sprintf_callback callback;
     msgpack_packer new_pk;
     msgpack_sbuffer sbuf;
+    uint8_t half = 0;
 
-    switch(*fmt)
+    do
     {
-        case 's':
-            ptr = va_arg(*args, void *);
-            if (ptr == NULL)
+        switch(*fmt)
+        {
+            case 'h': half = 1; // half prefix
+                break;
+            case 's':
+                ptr = va_arg(*ap, void *);
+                if (ptr == NULL)
+                    msgpack_pack_nil(pk);
+                else
+                    msgpack_pack_str_with_body(pk, ptr, strlen((const char *)ptr));
+                break;
+            case 'S': // not yet supported;
+                break;
+            case 'c':
+                u32 = va_arg(*ap, uint32_t);
+                msgpack_pack_str_with_body(pk, &u32, 1);
+                break;
+            case 'n': // null pointer
+                va_arg(*ap, void *); // ignore
                 msgpack_pack_nil(pk);
-            else
-                msgpack_pack_str_with_body(pk, ptr, strlen((const char *)ptr));
-            break;
-        case 'S': // not yet supported;
-            break;
-        case 'c':
-            u32 = va_arg(*args, uint32_t);
-            msgpack_pack_str_with_body(pk, &u32, 1);
-            break;
-        case 'n': // null pointer
-            va_arg(*args, void *); // ignore
-            msgpack_pack_nil(pk);
-            break;
-        case 'b': // boolean
-            u32 = va_arg(*args, uint32_t);
-            if (u32)
-                msgpack_pack_true(pk);
-            else
-                msgpack_pack_false(pk);
-            break;
-        case 'p': // binary 8/16/32
-            ptr = va_arg(*args, void *); // fetch pointer
-            u32 = va_arg(*args, uint32_t);   // fetch size
-            msgpack_pack_bin_with_body(pk, ptr, u32);
-            break;
-        case 'f': // float
-            break;
-        case 'e': // float64
-            break;
-        case 'i': //int
-            u32 = va_arg(*args, uint32_t);
-            msgpack_pack_int(pk, (int)u32);
-            break;
-        case 'u': //uint
-            u32 = va_arg(*args, uint32_t);
-            msgpack_pack_unsigned_int(pk, u32);
-            break;
-        case '!': // callback
-            callback = va_arg(*args, msgpack_sprintf_callback);
-            ptr = va_arg(*args, void *);
-            msgpack_sbuffer_init(&sbuf);
-            msgpack_packer_init(&new_pk, &sbuf, msgpack_sbuffer_write);
-            
-            callback(&new_pk, ptr);
+                break;
+            case 'd': // C23 boolean
+                u32 = va_arg(*ap, uint32_t);
+                if (u32)
+                    msgpack_pack_true(pk);
+                else
+                    msgpack_pack_false(pk);
+                break;
+            case 'p': // binary 8/16/32
+                ptr = va_arg(*ap, void *); // fetch pointer
+                u32 = va_arg(*ap, uint32_t);   // fetch size
+                msgpack_pack_bin_with_body(pk, ptr, u32);
+                break;
+            case 'f': // float
+                if (half)
+                {
+                    u16 = va_arg(*ap, uint16_t);    // BFLOAT16 .. easily mapped to float32
+                    f32 = 0.0;
+                    u32 = (uint32_t)(u16 << 16);
+#if (MSGPACK_ENDIAN_LITTLE_BYTE == 1)
+                    u32 = _msgpack_be32(u32);
+#endif
+                    memcpy(&f32, &u32, sizeof(u32));
+                }
+                else
+                {
+                    f32 = va_arg(*ap, float);
+                    msgpack_pack_float(pk, f32);
+                }
+                break;
+            case 'e': // float64
+                if (half)
+                {
+                    u16 = va_arg(*ap, uint16_t);
+                    f32 = 0.0;
+                    hf_to_float32(&f32, u16);
+                    msgpack_pack_float(pk, f32);
+                }
+                else
+                {
+                    f64 = va_arg(*ap, double);
+                    msgpack_pack_double(pk, f64);
+                }
+                break;
+            case 'i': //int
+                if (half)
+                    i32 = (int32_t)(va_arg(*ap, int16_t));
+                else
+                    i32 = va_arg(*ap, uint32_t);
+                msgpack_pack_int(pk, (int)i32);
+                break;
+            case 'u': //uint
+                if (half)
+                    u32 = (uint32_t)(va_arg(*ap, uint16_t));
+                else
+                    u32 = va_arg(*ap, uint32_t);
+                msgpack_pack_unsigned_int(pk, u32);
+                break;
+            case '!': // callback
+                callback = va_arg(*ap, msgpack_sprintf_callback);
+                ptr = va_arg(*ap, void *);
+                msgpack_sbuffer_init(&sbuf);
+                msgpack_packer_init(&new_pk, &sbuf, msgpack_sbuffer_write);
+                
+                callback(&new_pk, ptr);
 
-            // bulk copy
-            msgpack_sprintf_bulk(pk, &new_pk);
-            msgpack_sbuffer_free(&sbuf);
-            break;
-
-    }
+                msgpack_sprintf_bulk(pk, &new_pk);
+                msgpack_sbuffer_free(&sbuf);
+                break;
+        }
+        ++fmt;
+    } while(half != 0);
 
     return fmt;
 }
